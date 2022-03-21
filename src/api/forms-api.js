f1e130680f6bc1d196872a239d0c57f2c62068d9
stuartdambi@gmail.com <stuartdambi@gmail.com>
2022-03-21 10:58:24 +0300

    [add] created get Form details util function

--- src/api/forms-api.js
+++ src/api/forms-api.js
@@ -1,13 +1,29 @@
 class Forms {
   async createNewForm(formMeta) {
     try {
-      const response = await fetch(``, {
+      // const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_PROJECTS_URL}/forms/create`, {
+      const response = await fetch(`http://localhost:4000/api/v1/forms/create`, {
         method: 'POST',
         headers: {
           'Content-Type': 'Application/json',
           'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
         },
         body: JSON.stringify(formMeta)
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getFormDetails(formId) {
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_PROJECTS_URL}/api/v1/forms/${formId}`, {
+        headers: {
+          'Content-Type': 'Application/json',
+          'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        }
       });
       const data = await response.json();
       return data;
