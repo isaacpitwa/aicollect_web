e864ad31f9f0dfd95abd19958b5943d0c624e114
isaacpitwa <isaacpitwa256@gmail.com>
2022-06-13 09:55:18 +0300

    [ADD] Missing imports

--- src/pages/collections/index.js
+++ src/pages/collections/index.js
@@ -1,7 +1,7 @@
-import { AccountBillingSettings } from '../../../components/dashboard/account/account-billing-settings';
-import { DashboardLayout } from '../../../components/dashboard/dashboard-layout';
-import { AuthGuard } from '../../../components/authentication/auth-guard';
-import {useAuth} from '../../../hooks/use-auth'; 
+import { AccountBillingSettings } from '../../components/dashboard/account/account-billing-settings'; 
+import { DashboardLayout } from '../../components/dashboard/dashboard-layout';
+import { AuthGuard } from '../../components/authentication/auth-guard';
+import {useAuth} from '../../hooks/use-auth'; 
 function AccountBilling(props) {
     const { user } = useAuth();
     return <AccountBillingSettings user={user} />;
