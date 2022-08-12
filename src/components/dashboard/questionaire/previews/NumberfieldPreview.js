8d143d43ae1f83d31a205af9f37d8a3c9ca4794c
Isaac Pitwa <isaacpitwa256@gmail.com>
2022-08-12 12:11:55 +0300

    Code cleaning

--- src/components/dashboard/questionaire/previews/NumberfieldPreview.js
+++ src/components/dashboard/questionaire/previews/NumberfieldPreview.js
@@ -15,65 +15,64 @@ const NumberfieldPreview = (props) => {
 
     return (
         <>
-    { !multipleValues ?    
-     <Grid
-            item
-            xs={12}
-            md={6}
-            style={{ padding: '30px 20px' }}
-        >
-            <Typography
-                style={{ backgroundColor: '#5048E5', padding: '5px 10px', color: 'white', marginTop: '2px', borderRadius: '8px 8px 0px 0px' }}
-                size='small'
-            >
-                <strong>Preview</strong>
-            </Typography>
-            <Box
-                component="form"
-                style={{ padding: '20px', border: '1px #5048E5 solid', borderRadius: '0px 0px 8px 8px', marginTop: '-1px', minHeight: '200px' }}
-            >
-            <TextField
-                required={isRequired}
-                autoFocus
-                margin="dense"
-                id="label"
-                label={fieldLabel?fieldLabel:'Label'}
-                type="number"
-                size="small"
-                fullWidth
-                variant="outlined"
-                InputProps={{
-                    endAdornment: tooltip!=''?<GeneralTooltip tipData={tooltip}/>:false,
-                  }}
-            />
-                {fieldDescription!=''?
+            {!multipleValues ?
+                <Grid
+                    item
+                    xs={12}
+                    md={6}
+                    style={{ padding: '30px 20px' }}
+                >
                     <Typography
-                        style={{ fontSize: '14px', color: '#5048e598' }}
+                        style={{ backgroundColor: '#5048E5', padding: '5px 10px', color: 'white', marginTop: '2px', borderRadius: '8px 8px 0px 0px' }}
+                        size='small'
                     >
-                        <i>{fieldDescription}</i>
+                        <strong>Preview</strong>
                     </Typography>
-                :
-                    ''
-                }
-            </Box>
-        </Grid>: <MultipleValuesPreview {...props}  component={<TextField
-                                        required={isRequired}
-                                        autoFocus
-                                        margin="dense"
-                                        id="label"
-                                        label={fieldLabel?fieldLabel:'Label'}
-                                        type="number"
-                                        size="small"
-                                        fullWidth
-                                        variant="outlined"
-                                        InputProps={{
-                                            endAdornment: tooltip!=''?<GeneralTooltip tipData={tooltip}/>:false,
-                                        }}
-                                        
-                                    />}/>}
+                    <Box
+                        component="form"
+                        style={{ padding: '20px', border: '1px #5048E5 solid', borderRadius: '0px 0px 8px 8px', marginTop: '-1px', minHeight: '200px' }}
+                    >
+                        <TextField
+                            required={isRequired}
+                            autoFocus
+                            margin="dense"
+                            id="label"
+                            label={fieldLabel ? fieldLabel : 'Label'}
+                            type="number"
+                            size="small"
+                            fullWidth
+                            variant="outlined"
+                            InputProps={{
+                                endAdornment: tooltip != '' ? <GeneralTooltip tipData={tooltip} /> : false,
+                            }}
+                        />
+                        {fieldDescription != '' ?
+                            <Typography
+                                style={{ fontSize: '14px', color: '#5048e598' }}
+                            >
+                                <i>{fieldDescription}</i>
+                            </Typography>
+                            :
+                            ''
+                        }
+                    </Box>
+                </Grid> : <MultipleValuesPreview {...props} component={<TextField
+                    required={isRequired}
+                    autoFocus
+                    margin="dense"
+                    id="label"
+                    label={fieldLabel ? fieldLabel : 'Label'}
+                    type="number"
+                    size="small"
+                    fullWidth
+                    variant="outlined"
+                    InputProps={{
+                        endAdornment: tooltip != '' ? <GeneralTooltip tipData={tooltip} /> : false,
+                    }}
+
+                />} />}
         </>
     )
 }
 
 export default NumberfieldPreview
-                    
