edf7ef45cbbb39eeda97141514fbd169e3905446
Isaac Pitwa <isaacpitwa256@gmail.com>
2022-08-18 14:09:07 +0300

    Use Existing questions for Calculated Field questionaire

--- src/components/dashboard/questionaire/components/calculatedFormulaInput.js
+++ src/components/dashboard/questionaire/components/calculatedFormulaInput.js
@@ -4,12 +4,15 @@ import {
     Fade,
     List,
     ListItemButton,
+    ListSubheader,
     Paper,
     TextField,
+    Typography,
 } from '@mui/material'
 import { Box } from '@mui/system'
-import React, { createRef, useCallback } from 'react'
-import { atom,RecoilRoot, useRecoilState, useSetRecoilState } from 'recoil'
+import React, { createRef, useCallback, useContext } from 'react'
+import { atom, RecoilRoot, useRecoilState, useSetRecoilState } from 'recoil'
+import { FormContext } from '../context'
 
 const suggestionsState = atom({
     key: 'Band Suggestions Visible?',
@@ -25,6 +28,12 @@ const inputState = atom({
 function Suggestions({ field }) {
     const [hasSuggestions, setHasSuggestions] = useRecoilState(suggestionsState)
     const [value, setValue] = useRecoilState(inputState)
+    const {
+        isLoaded,
+        formData,
+        formFieldValues,
+    } = useContext(FormContext)
+
     const handleClick = useCallback(
         (band) => {
             setValue(value + band)
@@ -38,10 +47,54 @@ function Suggestions({ field }) {
         field.current?.focus()
     }, [field, setHasSuggestions])
 
+    console.log('formData: ', formData);
+    console.log('formFieldValues: ', formFieldValues);
     return (
         <ClickAwayListener onClickAway={handleClickAway}>
             <Fade in={hasSuggestions}>
                 <List component={Paper} sx={{ mt: 1 }}>
+                    {
+                        isLoaded && formData ?
+                            formData.formFields.map((sectionData, index) => {
+                                return (
+                                    <>
+                                        <ListSubheader>{sectionData.label}</ListSubheader>
+                                        {
+                                            sectionData.components.map((formfield, index2) => {
+                                                if (formfield.type === "sub-section") {
+                                                    return (
+                                                        <>
+                                                            <ListSubheader>{'-> ' + formfield.label}</ListSubheader>
+                                                            {formfield.components.map((subsection, index3) => {
+                                                                return (
+                                                                    <ListItemButton
+                                                                        key={index3}
+                                                                        onClick={() => handleClick(subsection.label.replace(/\s/g, ''))}
+                                                                    >
+                                                                        {subsection.label}
+                                                                    </ListItemButton>
+                                                                )
+                                                            })
+                                                            }
+                                                        </>
+                                                    )
+                                                }
+                                                else {
+                                                    return (<ListItemButton key={index2} onClick={() => handleClick(formfield.label.replace(/\s/g, ''))}>
+                                                        {formfield.label}
+                                                    </ListItemButton>)
+                                                }
+                                            }
+                                            )
+                                        }
+                                    </>
+
+                                )
+                            }
+                            )
+                            : <Typography>Loading...</Typography>
+                    }
+
                     <ListItemButton onClick={() => handleClick('Mötley Crüe')}>
                         Mötley Crüe
                     </ListItemButton>
@@ -73,7 +126,7 @@ function SuggestionsField() {
 
     return (
 
-        <Box display="inline-block" sx={{width:'100%'}}>
+        <Box display="inline-block" sx={{ width: '100%' }}>
             <TextField
                 inputRef={textFieldRef}
                 label="Type @ to select question"
@@ -93,7 +146,7 @@ function SuggestionsField() {
     )
 }
 
-export default function  CalculatedFormulaInput (){
+export default function CalculatedFormulaInput() {
     return (
         <RecoilRoot>
             <CssBaseline />
