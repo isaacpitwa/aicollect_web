289b9aac405f2f2e3ccf64491fdc2683359df89e
Isaac Pitwa <isaacpitwa256@gmail.com>
2022-08-22 00:18:08 +0300

    Clean x-data-grid mui imports

--- src/components/dashboard/questionaire/FormRender/FormField/dataGridField.js
+++ src/components/dashboard/questionaire/FormRender/FormField/dataGridField.js
@@ -3,7 +3,8 @@ import formStyles from '../../styles/FormStyles';
 import { smallBtns } from '../../styles/FormStyles';
 import {
     Grid,
-    Typography
+    Typography,
+    Box
 } from "@mui/material";
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import EditIcon from '@mui/icons-material/Edit';
@@ -13,7 +14,6 @@ import DataGridDialog from '../../dialogs/DataGridDialog';
 import { DescriptionCard } from '../../utils';
 import GeneralTooltip from '../../previews/GeneralTooltip';
 import FormField from '.';
-import { Box } from '@mui/system';
 import MultipleValuesField from './MultipleValuesField';
 
 /**
