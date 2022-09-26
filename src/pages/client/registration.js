41c15e3cda87189959c447a4d6f524a6c0dd3fff
Isaac Pitwa <isaacpitwa256@gmail.com>
2022-09-26 09:55:22 +0300

    clean Component code

--- src/pages/client/registration.js
+++ src/pages/client/registration.js
@@ -47,28 +47,7 @@ const RegisterClient = () => {
       <Head>
         <title>Client Information | AiCollect</title>
       </Head>
-
       <RootStyle sx={{height:'100vh'}}>
-        {/* <MHidden width="mdDown">
-          <SectionStyle sx={{
-            backgroundColor: '#1971b9', margin: 0,
-            maxWidth: '50%',
-            borderRadius: 0, display: 'flex', flexDirection: 'column',
-            alignItems: 'center', justifyContent: 'center', color: '#282BD3', padding: '0 12%'
-          }}>
-            <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
-              <img src="/images/illustration.svg" alt="aicollect-login" style={{ width: '21rem' }} />
-            </Box>
-
-            <Typography variant="h5" sx={{ color: 'white', fontWeight: '600', mt: '32px' }}>
-              Go beyond data collection achieve amazing things with your data
-            </Typography>
-            <Typography variant="p" sx={{ color: '#282BD3', fontWeight: '400', color: 'white', fontSize: '14px', mt: '4px' }}>
-              Deploy custom forms to field teams to conduct assessments for your business. Transform your data in powerful insights.
-              its 100% customizable to suit your specific needs.
-            </Typography>
-          </SectionStyle>
-        </MHidden> */}
         <Container>
           <ContentStyle sx={{maxWidth:'60%'}}>
             <Stack sx={{ mb: 2,textAlign:'center' }}>
